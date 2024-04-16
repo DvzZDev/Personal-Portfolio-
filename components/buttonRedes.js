22867e494a228cb8572e48831b91cdae66af9a86
David <estebandavid578@gmail.com>
2024-04-16 23:30:27 +0200

    update buttons

--- components/buttonRedes.js
+++ components/buttonRedes.js
@@ -3,11 +3,11 @@ import React from "react";
 
 function btnredes({ href, nombre, imagen }) {
   return (
-    <button className="bg-transparent hover:bg-blue-400 text-white font-semibold hover:text-white py-2 px-2 border border-blue-400 hover:border-transparent rounded-2xl mt-1 flex flex-row transform active:scale-y-75 transition-transform mr-2 md:text-lg">
+    <button className="bg-transparent hover:bg-blue-400 text-white font-semibold hover:text-white py-2 px-2 border border-blue-400 hover:border-transparent rounded-lg mt-1 flex flex-row transform active:scale-y-75 transition-trans form mr-2 md:text-lg">
       <a href={href} target="_blank" rel="noopener noreferrer">
         {nombre}
       </a>
-      <img src={imagen} alt={nombre} className="h-6 ml-1"></img>
+      <img src={imagen} alt={nombre} className="h-7 ml-1"></img>
     </button>
   );
 }
