147e4609b35c966d2e46e415c656f8a3b77dbee9
David <estebandavid578@gmail.com>
2024-04-19 20:54:37 +0200

    Vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,11 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }) {
   return (
-    (
-      <head>
-       
-      </head>
-    ),
+    (<head></head>),
     (
       <html className="scroll-smooth" lang="en">
         <body className={inter.className}>{children}</body>
