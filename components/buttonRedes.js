6310931ef2e7744710a63cb989c1eb226b74093a
David <estebandavid578@gmail.com>
2024-04-20 02:55:54 +0200

    Update buttonRedes.js

--- components/buttonRedes.js
+++ components/buttonRedes.js
@@ -9,14 +9,15 @@ const rubik = Rubik({
 
 function BtnRedes({ href, nombre, imagen }) {
   return (
-    <button
-      className={`${rubik.className} items-center bg-transparent hover:bg-blue-400 text-white font-semibold hover:text-white  px-2 border border-blue-400 hover:border-transparent rounded-lg mt-1 flex flex-row transform active:scale-y-75 transition-trans form mr-2 md:p-1 `}
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`${rubik.className} items-center bg-transparent hover:bg-blue-400 text-white font-semibold hover:text-white px-2 border border-blue-400 hover:border-transparent rounded-lg mt-1 flex flex-row transform active:scale-y-75 transition-transform mr-2 md:p-1`}
     >
-      <a href={href} target="_blank" rel="noopener noreferrer">
-        {nombre}
-      </a>
-      <img src={imagen} alt={nombre} className="ml-1 h-4 md:h-6"></img>
-    </button>
+      {nombre}
+      <img src={imagen} alt={nombre} className="ml-1 h-4 md:h-6" />
+    </a>
   );
 }
 
